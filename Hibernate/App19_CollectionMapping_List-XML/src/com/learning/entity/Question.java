fab8044716edba689180388d3aa859383a18b67d
Ankit <ankitkumargcet@gmail.com>
2019-06-11 11:12:55 +0530

    First Commit

---
+++ Hibernate/App19_CollectionMapping_List-XML/src/com/learning/entity/Question.java
@@ -0,0 +1,34 @@
+package com.learning.entity;
+
+import java.util.List;
+
+public class Question {
+	private int qid;
+	private String qname;
+	private List<String> answers;
+
+	public int getQid() {
+		return qid;
+	}
+
+	public void setQid(int qid) {
+		this.qid = qid;
+	}
+
+	public String getQname() {
+		return qname;
+	}
+
+	public void setQname(String qname) {
+		this.qname = qname;
+	}
+
+	public List<String> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<String> answers) {
+		this.answers = answers;
+	}
+
+}
---
+++ Hibernate/App19_CollectionMapping_List-XML/src/com/learning/test/Test.java
@@ -0,0 +1,42 @@
+package com.learning.test;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.learning.entity.Question;
+import com.learning.util.HibernateUtil;
+
+public class Test {
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		
+		ArrayList<String> list1 = new ArrayList<String>();
+		list1.add("Java is a programming language");    
+	    list1.add("Java is a platform");
+	    list1.add("Java is used globally");
+	    
+	    ArrayList<String> list2 = new ArrayList<String>();
+	    list2.add("Servlet is an Interface");    
+	    list2.add("Servlet is an API");
+	    
+	    Question ques1 = new Question();
+	    ques1.setQname("What is Java");
+	    ques1.setAnswers(list1);
+	    
+	    Question ques2 = new Question();
+	    ques2.setQname("What is Servlet");
+	    ques2.setAnswers(list2);
+	    
+	    session.persist(ques1);
+	    session.persist(ques2);
+	    
+	    tx.commit();
+	    System.out.println("Saved Sucessfully");
+	    session.close();
+	    
+	}
+
+}
