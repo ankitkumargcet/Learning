ea5dd5a1539f0275e223e543b21c6a5390c67ca1
Ankit <ankitkumargcet@gmail.com>
2019-06-13 13:48:03 +0530

    First Commit

---
+++ Hibernate/App29_AM_ManyToMany-XML/src/com/learning/entity/Answer.java
@@ -0,0 +1,43 @@
+package com.learning.entity;
+
+import java.util.List;
+
+public class Answer {
+	private int id;
+	private String aname;
+	private String postedBy;
+	private List<Question> questions;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAname() {
+		return aname;
+	}
+
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+
+	public String getPostedBy() {
+		return postedBy;
+	}
+
+	public void setPostedBy(String postedBy) {
+		this.postedBy = postedBy;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+
+}
